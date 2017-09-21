cc6e3b17bef5bd188b7d9297a58c27016ed04e8e
Javiz7 <31923944+Javiz7@users.noreply.github.com>
2017-09-21 12:54:51 +0200

    Add files via upload

---
+++ Second.java
@@ -0,0 +1,12 @@
+public class Second {
+	public static void main(String[] args) {
+		System.out.println(".");
+		System.out.println(".");
+		System.out.println(".");
+		System.out.println(".");
+		System.out.println(" ");
+		System.out.println(" ");
+		System.out.println("2");
+		System.out.println(":");
+	}
+}
